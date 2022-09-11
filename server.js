a56d7f23f5301e1f6505ba1c7d386565f4958467
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-09-11 09:52:55 +0000

    update fix a problem

--- server.js
+++ server.js
@@ -6,25 +6,24 @@ require("dotenv").config();
 const controller_articles = require("./controller/articles");
 const scraper = require("./controller/scraping");
 app.use(cors());
+app.use(Express.json());
 var articles = {message: "waiting a lot.."};// varaible for articles
-(async ()=> {
-    await controller_articles.getArticles().then((data) => {
-        articles = data; 
-    }).catch((error)=> {console.log(error)});
-    setInterval(async() => {
-        await controller_articles.getArticles().then((data) => {
-            articles = data; 
-        }).catch((error)=> {console.log(error)});
-    }, 60000 * process.env.MINS);
-})();
+// (async ()=> {
+//     await controller_articles.getArticles().then((data) => {
+//         articles = data; 
+//     }).catch((error)=> {console.log(error)});
+//     setInterval(async() => {
+//         await controller_articles.getArticles().then((data) => {
+//             articles = data; 
+//         }).catch((error)=> {console.log(error)});
+//     }, 60000 * process.env.MINS);
+// })();
 // to get the posts
-app.get("/", async (req, res)=> {
-    res.send(articles); 
-});
+// app.get("/", async (req, res)=> {
+//     res.send(articles); 
+// });
 // to get the text and code html in the post
 app.post("/post", async (req, res)=> {
-    console.log(req.body.url);
-    console.log(req.body.authentication);
     if (req.body.url === undefined || req.body.authentication === undefined) {
         res.send({
             message: "the url and the authentication are required.",
